0b581e10cc774849050b93214e4fbc2733e5a43b
arefogit <aref.ahmadi.ce@gmail.com>
2024-05-14 23:16:21 +0330

    created core/urls.py and signup.html

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from item.models import Category, Item
-
+from .forms import SignupForm
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
@@ -15,3 +15,10 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    form = SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
