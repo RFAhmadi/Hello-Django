b3997c338bdf2deb4269de7cee4aaed61c08a4c7
arefogit <aref.ahmadi.ce@gmail.com>
2024-07-06 23:28:46 +0330

    added forms.py & form.html in templates\item ; changed urls.py views.py in item ; base.html in core

--- item/views.py
+++ item/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from .forms import NewItemForm
 from .models import Item
 
 def detail(request, pk):
@@ -11,4 +13,12 @@ def detail(request, pk):
     return render(request, 'item/detail.html', {
         'item': item,
         'related_items': related_items
+    })
+
+@login_required
+def new(request):
+    form = NewItemForm()
+
+    return render(request, 'item/form.html', {
+        'form': form
     })
